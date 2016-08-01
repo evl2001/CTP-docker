9b09992772234d6e7bab49ae3ae31e73bc040205
EM LoCastro <evl2001@med.cornell.edu>
2016-08-01 12:21:45 -0400

    rebundled CTP install with pixel anon tools

--- alpine/container_build.sh
+++ alpine/container_build.sh
@@ -23,5 +23,5 @@ CONTAINERVOL=/install/CTP/roots/DirectoryStorageService
 #Container ID
 CONTAINERNAME=CTP_alpine_`date +%s`
 
-docker build -t "${TAGID}" ./
-docker run -v ${LOCALVOL}:${CONTAINERVOL} -p 2104:2104 -p 1090:1080 --rm --name ${CONTAINERNAME} ${TAGID}
+#docker build -t "${TAGID}" ./
+docker run -v ${LOCALVOL}:${CONTAINERVOL} -p 2104:2104 -p 7104:7104 -p 1090:1080 --rm --name ${CONTAINERNAME} ${TAGID}
